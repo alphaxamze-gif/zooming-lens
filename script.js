ee783403ce64f8d8fa7d4ac90d0296d8a0b2f6ba
alphaxamze-gif <alphaxamze@gmail.com>
2026-02-19 13:14:09 +0300

    add java

--- script.js
+++ script.js
@@ -10,6 +10,15 @@ const y = pos.y;
 glass.style.left = x + 'px';
 glass.style.top = y + 'px';
 
-glass.style.backgroundImage = 'url($(img.src'});
-glass.style
-}
+glass.style.backgroundImage = 'url(${img.src})';
+glass.style.backgroundsize = '$'{img.width * 1.5}px ${img.height * 1.5}px;
+glass.style.backgroundposition = '-${x * 1.5 - glass.offsetHeight/2}px';
+});
+
+function getCursorpos(element, e){
+    const rect = element.getBoundingClientRect();
+    return {
+        x: e.clientX - react.left,
+        y:e.clientY - react.top
+    };
+} 
