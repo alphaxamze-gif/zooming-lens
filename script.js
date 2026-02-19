6e5ce49172a4cc35c04459acfef1293e68f80992
alphaxamze-gif <alphaxamze@gmail.com>
2026-02-19 12:08:49 +0300

    add java script file

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 document.querySelector(''.magnify-content').addEventListener
 ('mousemove', function(e) {
 const glass = this.querySelector('.magnify-glass');
-const img = this.querySelector
+const img = this.querySelector('img');
+
+const pos = getCursorpos(this, e);
+const x = pos.x;
+const y = pos.y;
+
+glass.style.left = x + 'px';
+glass.style.top = y + 'px';
+
+glass.style.backgroundImage = 'url($(img.src'});
+glass.style
 }
